1a9f4b13c902573b6d9d7bdb951f35de1b22dccc
robin larsson <rrr2009@live.se>
2016-11-09 13:01:30 +0100

    fräshare

--- Inlamningsuppgift2/Inlamningsuppgift2/Program.cs
+++ Inlamningsuppgift2/Inlamningsuppgift2/Program.cs
@@ -1,23 +1,18 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Data;
 using System.Data.SqlClient;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Inlamningsuppgift2
 {
-    class Program
+    internal class Program
     {
-        static void Main(string[] args)
+        private static void Main(string[] args)
         {
             int counter = 0;
             int tal = 0;
-            
+
             do
             {
-                Console.WriteLine("Type 1 to insert a customer \nType 2 to insert a product  "+
+                Console.WriteLine("Type 1 to insert a customer \nType 2 to insert a product  " +
                     "\nType 3 to update product price at a specific product \nType 4 to view ContactNameChangedTrigger");
                 try
                 {
@@ -33,20 +28,20 @@ namespace Inlamningsuppgift2
             while (counter == 0);
             counter = 0;
 
-            if (tal == 1) {
+            if (tal == 1)
+            {
                 do
                 {
-                    Console.WriteLine("Enter your Customer ID:");
-                    string customerID = Console.ReadLine();
-                    Console.WriteLine("Enter your Company name:");
-                    string companyName = Console.ReadLine();
-                    Console.WriteLine("Enter your Contact name:");
-                    string contactName = Console.ReadLine();
-                    Console.WriteLine("Enter your Phone number:");
-                    int phoneNumber = int.Parse(Console.ReadLine());
                     try
                     {
-
+                        Console.WriteLine("Enter your Customer ID:");
+                        string customerID = Console.ReadLine();
+                        Console.WriteLine("Enter your Company name:");
+                        string companyName = Console.ReadLine();
+                        Console.WriteLine("Enter your Contact name:");
+                        string contactName = Console.ReadLine();
+                        Console.WriteLine("Enter your Phone number:");
+                        int phoneNumber = int.Parse(Console.ReadLine());
 
                         string cns = @"Server=(localdb)\MSSQLLocalDB;Database=NORTHWND;Trusted_Connection=Yes";
                         SqlConnection cn = new SqlConnection(cns);
@@ -66,26 +61,23 @@ namespace Inlamningsuppgift2
                     }
                     catch
                     {
-                        Console.WriteLine("One of the fields where in a incorrect format, please enter the information again");
+                        Console.WriteLine("One of the fields where in a incorrect format,");
+                        Console.WriteLine("please enter the information again");
                         Console.WriteLine("");
                     }
                 }
                 while (counter == 0);
             }
-
             else if (tal == 2)
             {
-
                 do
                 {
-
-                    Console.WriteLine("Enter your Product name:");
-                    string productName = Console.ReadLine();
-                    Console.WriteLine("Enter the Price :");
-                    int unitPrice = int.Parse(Console.ReadLine());
                     try
                     {
-
+                        Console.WriteLine("Enter your Product name:");
+                        string productName = Console.ReadLine();
+                        Console.WriteLine("Enter the Price :");
+                        int unitPrice = int.Parse(Console.ReadLine());
 
                         string cns = @"Server=(localdb)\MSSQLLocalDB;Database=NORTHWND;Trusted_Connection=Yes";
                         SqlConnection cn = new SqlConnection(cns);
@@ -103,16 +95,15 @@ namespace Inlamningsuppgift2
                     }
                     catch
                     {
-                        Console.WriteLine("One of the fields where in a incorrect format, please enter the information again");
+                        Console.WriteLine("One of the fields where in a incorrect format,");
+                        Console.WriteLine("please enter the information again");
                         Console.WriteLine("");
                     }
                 }
                 while (counter == 0);
-
             }
             else if (tal == 3)
             {
-                
                 using (SqlConnection connection = new SqlConnection(@"Server=(localdb)\MSSQLLocalDB;Database=NORTHWND;Trusted_Connection=Yes"))
                 using (SqlCommand command = connection.CreateCommand())
                 {
@@ -123,21 +114,16 @@ namespace Inlamningsuppgift2
                         while (reader.Read())
                         {
                             Console.WriteLine(reader.GetInt32(0) + ": " + reader.GetString(1));
-                            
                         }
                     }
-
                 }
-                Console.WriteLine(" ");
-                Console.WriteLine("Type the ID of the product you want to edit");
-                int selectedID = int.Parse(Console.ReadLine());
-                Console.WriteLine("Enter the new price: ");
-                int price = int.Parse(Console.ReadLine());
-
                 try
                 {
-
-
+                    Console.WriteLine(" ");
+                    Console.WriteLine("Type the ID of the product you want to edit");
+                    int selectedID = int.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter the new price: ");
+                    int price = int.Parse(Console.ReadLine());
                     string cns = @"Server=(localdb)\MSSQLLocalDB;Database=NORTHWND;Trusted_Connection=Yes";
                     SqlConnection cn = new SqlConnection(cns);
                     cn.Open();
@@ -155,21 +141,20 @@ namespace Inlamningsuppgift2
                     Console.WriteLine("One of the fields where in a incorrect format, please enter the information again");
                     Console.WriteLine("");
                 }
-               
             }
-            else if (tal ==4)
+            else if (tal == 4)
             {
                 using (SqlConnection connection = new SqlConnection(@"Server=(localdb)\MSSQLLocalDB;Database=NORTHWND;Trusted_Connection=Yes"))
                 using (SqlCommand command = connection.CreateCommand())
                 {
                     command.CommandText = "Select * from CustomerChanges";
-                    
+
                     connection.Open();
                     using (var reader = command.ExecuteReader())
                     {
                         while (reader.Read())
                         {
-                            Console.WriteLine("ID: " + reader.GetString(0)+ "    Old contact name: " +reader.GetString(1) +
+                            Console.WriteLine("ID: " + reader.GetString(0) + "    Old contact name: " + reader.GetString(1) +
                                 "|   New contact name: " + reader.GetString(2));
                         }
                         Console.WriteLine("");
@@ -178,24 +163,11 @@ namespace Inlamningsuppgift2
                     connection.Close();
                 }
                 Console.ReadKey();
-               
             }
             else
             {
                 Console.WriteLine("That's not a valid command");
-
-
-
-
             }
-           
         }
-            
-
-        }
-   
-
-
-
     }
-
+}
